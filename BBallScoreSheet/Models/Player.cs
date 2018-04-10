8defb15e2df4140fb4981c6d7f5c79ae3912aa7a
catpow22 <35718095+catpow22@users.noreply.github.com>
2018-04-09 22:17:59 -0700

    player class

---
+++ BBallScoreSheet/Models/Player.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBallScoreSheet.Models
+{
+    public class Player : ObservableObject
+    {
+        #region Fields
+
+        private int _totalPoints;
+        private string _playertName;
+        private int _twoPointFG;
+
+        #endregion // Fields
+
+        #region Properties
+
+        public int TotalPoints
+        {
+            get { return _totalPoints; }
+            set
+            {
+                if (value != _totalPoints)
+                {
+                    _totalPoints = value;
+                    OnPropertyChanged("TotalPoints");
+                }
+            }
+        }
+
+        public string PlayerName
+        {
+            get { return _playertName; }
+            set
+            {
+                if (value != _playertName)
+                {
+                    _playertName = value;
+                    OnPropertyChanged("PlayerName");
+                }
+            }
+        }
+
+        public int TwoPointFG
+        {
+            get { return _twoPointFG; }
+            set
+            {
+                if (value != _twoPointFG)
+                {
+                    _twoPointFG = value;
+                    OnPropertyChanged("TwoPointFG");
+                }
+            }
+        }
+
+        #endregion // Properties    
+    }
+ }
---
+++ BballScoreSheetConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BballScoreSheetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+
+            Console.ReadLine();
+        }
+
+
+        static int PlusTwoFG(int scoreSoFar)
+        {
+            return scoreSoFar + 1;
+        }
+        
+    }
+
+
+}
